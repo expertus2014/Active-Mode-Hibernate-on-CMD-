2889218f7f00ef207a73db8dc82fece639b20c88
Aby-Profe <77512064+expertus2014@users.noreply.github.com>
2023-12-18 04:05:10 -0800

    Update README.bat

--- README.bat
+++ README.bat
@@ -1,4 +1,4 @@
-# Active-Mode-Hibernate-on-CMD-
-Comment Activer le mode mise en veille prolonger dans windows avec CMD
+:: # Active-Mode-Hibernate-on-CMD-
+:: Comment Activer le mode mise en veille prolonger dans windows avec CMD
 
 >powercfg -hibernate on
